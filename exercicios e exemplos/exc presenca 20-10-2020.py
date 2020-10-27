6e1b4a24f37dd1fae86a40aa05d36407a6bbb2fe
Priscila Da Dalt <priscila_dd@yahoo.com.br>
2020-10-27 19:29:07 -0300

    exercicio de 20/10/2020

---
+++ exercicios e exemplos/exc presenca 20-10-2020.py
@@ -0,0 +1,9 @@
+def hash(palavra, tamanho_tabela):
+    soma = 0
+    for pos in range(len(palavra)):
+        soma = soma + ord(palavra[pos])
+    return soma % tamanho_tabela
+
+print(hash('impacta',23))
+print(hash('faculdade',23))
+print(hash('atividade',23))
